0ff3087beba025d488b45b2102717ffe91c85251
Krishna Sharma <Ks9192174414@gmail.com>
2023-09-20 18:14:59 +0530

    change the model of user

--- models/User.js
+++ models/User.js
@@ -1,22 +1,28 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    require: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      require: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+    },
+    mobile: {
+      type: String,
+    },
   },
-  email: {
-    type: String,
-    unique: true,
-    require: true,
-  },
-  password: {
-    type: String,
-    require: true,
-  },
-  mobile: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
 
 module.exports = mongoose.model("user", userSchema);
